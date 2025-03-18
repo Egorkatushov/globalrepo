39102ca94df261a251df56bc2196a1476a14726b
Katushov Egor <e.katushov@mail.ru>
2025-03-18 21:39:16 +0400

    Сделал так что бы выводилесь все элементы, не только 10.

--- first-project/gitdz.cpp
+++ first-project/gitdz.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int fib(int n){ 
-if (n==0 || n==1){
+if (n==0){
 return 0;
 }
-if ( n==2){
+if ( n==1){
 return 1;
 }
 return fib(n-1)+fib(n-2);
@@ -14,7 +14,8 @@ return fib(n-1)+fib(n-2);
 
 int main(){
 int n=10;
-cout<<fib(n)<<endl; 
+for (int i=0;i<n;i++)
+cout<<fib(i)<<endl; 
 cout<<"Hello world!"<<endl; 
 return 0;
 }
